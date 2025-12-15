ef387c2df390aa2db57d3dac82e505f323b10755
AnouarY02 <anouary02@gmail.com>
2025-12-15 19:48:34 +0100

    Delete shared directory

--- shared/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './validation/schemas';
-export * from './constants/errors';
-export * from './constants/index';
-export * from './utils/apiClient';
-export * from './utils/authUtils';
-export * from './utils/dates';
