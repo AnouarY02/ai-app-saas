d8adc7b4cf7c7401c698c7f8ed2f6dbbeddfdf90
AnouarY02 <anouary02@gmail.com>
2025-12-15 18:44:27 +0100

    WF3 auto-commit → shared/index.ts

---
+++ shared/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './validation/schemas';
+export * from './constants/errors';
+export * from './constants/index';
+export * from './utils/apiClient';
+export * from './utils/authUtils';
+export * from './utils/dates';
