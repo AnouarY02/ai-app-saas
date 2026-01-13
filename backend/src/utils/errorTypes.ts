4ea0328414fcead6040e63ef406dc146da3b29f4
AnouarY02 <anouary02@gmail.com>
2026-01-13 15:31:59 +0100

    Delete backend directory

--- backend/src/utils/errorTypes.ts
+++
@@ -1,9 +0,0 @@
-// Shared API error type
-export class ApiError extends Error {
-  statusCode: number;
-  constructor(message: string, statusCode: number = 400) {
-    super(message);
-    this.statusCode = statusCode;
-    Object.setPrototypeOf(this, ApiError.prototype);
-  }
-}
