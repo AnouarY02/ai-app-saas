88ee38ab5197c572fe6c6e05ec91dfcaf5d0e45d
AnouarY02 <anouary02@gmail.com>
2026-01-13 15:32:21 +0100

    Delete runs directory

--- runs/build-1768144029780/app/api/clubs/[clubId]/wallets/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getWalletsByClub } from "../../../../../lib/data/wallet-store";
-
-export async function GET(_: NextRequest, { params }: { params: { clubId: string } }) {
-  const wallets = await getWalletsByClub(params.clubId);
-  return NextResponse.json(wallets);
-}
-
